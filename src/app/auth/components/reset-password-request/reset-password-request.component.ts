3cf89c2abaf4f7665746d6858c4539544b7b2b4e
Fernando Emanuel <78227397+femm34@users.noreply.github.com>
2025-04-27 17:27:55 -0600

    initial commit

---
+++ src/app/auth/components/reset-password-request/reset-password-request.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-reset-password-request',
+  imports: [],
+  templateUrl: './reset-password-request.component.html',
+  styleUrl: './reset-password-request.component.css'
+})
+export class ResetPasswordRequestComponent {
+
+}
